89a57fa63c7142ce5b399a00e552936a03c5e40e
Purvi Singh <spurvi610@gmail.com>
2025-05-15 14:28:05 +0530

    updated constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,7 +2,7 @@ const isDevelopment = window.location.hostname === 'localhost';
 
 const BASE_URL = isDevelopment
   ? "http://localhost:8080/api/v1"
-  : "https://your-backend-url.onrender.com/api/v1";
+  : "https://hirevibe.onrender.com/api/v1";
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
